91059159dbe7d136c0011b4c90ddd9eced658962
dioufr313d <dioufr313@gmail.com>
2026-02-06 10:01:07 +0100

    init projet

---
+++ src/main/java/sn/edu/isepat/vente_chaussure_rdiouf/VenteChaussureRdioufApplication.java
@@ -0,0 +1,13 @@
+package sn.edu.isepat.vente_chaussure_rdiouf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenteChaussureRdioufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenteChaussureRdioufApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/isepat/vente_chaussure_rdiouf/VenteChaussureRdioufApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.isepat.vente_chaussure_rdiouf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenteChaussureRdioufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
